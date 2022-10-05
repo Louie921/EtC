cb72fca8e6123f57b301507b9ec4da6b3ea2a67c
{kunii} <{ruiruihawks1212@ezweb.ne.jp}>
2022-10-05 18:38:06 +0900

    update

--- EtC.c
+++ EtC.c
@@ -80,7 +80,7 @@ void makePt(int Pt[], int weight, int row, matrix mH, int H[mH.column][mH.weight
 
 void xor(int M,int X1[M],int X2[M],int X[M]){//排他的論理和 
     for(int m=0;m<M;m++){
-        X[m]=(X1[m]+X2[m])%2;
+        X[m]=X1[m]^X2[m];
     }
 }
 
@@ -94,19 +94,19 @@ void mkplain(int M,int X[M]){//平文ブロック生成
     }
 }
 
-double correlation(int key[PACKNUM],int encr[PACKNUM]){
+double correlation(int M,int key[M],int encr[M]){
   int n;
   int err=0;
 
   double p;
 
-  for(n=0;n<PACKNUM;n++){
+  for(n=0;n<M;n++){
     if(key[n] != encr[n]){
       err++;
     }
   }
 
-  p = ((double)err) / ((double)PACKNUM);
+  p = ((double)err) / ((double)M);
 
   return p;
 }
@@ -130,32 +130,46 @@ int main(void){
     srand((unsigned int) time(NULL));
     init_genrand((unsigned)time(NULL));
 
-    int N;//ブロック数
-    int M;//ブロック長
-    int IV[M];//初期ベクトル
-    int X[N][M];//平文ブロック
+    int N=3;//ブロック数
+    int M=8;//ブロック長
+    // int IV[M];//初期ベクトル
+    int X[N][M];//平文ブロック X0初期ベクトル
     int EX[N][M];//暗号ブロック
-    int n,m,js;
+    int n,m,j,k;
 
     // 平文ブロック生成
-    for(n=0;n<N;n++){
+    for(n=0;n<N+1;n++){
         mkplain(M,X[n]);
     }
-    // 初期ベクトル生成
-    mkplain(M,IV);
+    for(n=0;n<N+1;n++){
+        printf("%d\n",n);
+        for(m=0;m<M;m++){
+            printf("\t%d\n",X[n][m]);
+        }
+    }
+    // // 初期ベクトル生成
+    // mkplain(M,IV);
 
     // 暗号ブロック生成
-    xor(M,IV,X[0],EX[0]);
-    for(n=1;n<N;n++){
+    for(m=0;m<M;m++){
+        EX[0][m]=X[0][m];
+    }
+    for(n=1;n<N+1;n++){
         xor(M,EX[n-1],X[n],EX[n]);
+    }
+    for(n=0;n<N+1;n++){
+        printf("%d\n",n);
+        for(m=0;m<M;m++){
+            printf("\t%d\n",EX[n][m]);
+        }
     }
 
     //Hの作成
     matrix mP0;
-    mP0.weight=10;
+    mP0.weight=2;
     int R=0.5;//圧縮率
     mP0.row=M*R;
-    mP0.column=mP0.row/10;
+    mP0.column=mP0.row/2;
     int P0[mP0.column][mP0.weight];
     int C;//部分行列の数
     C=mP0.row/mP0.column;
@@ -174,18 +188,30 @@ int main(void){
         makeP(array, mP0, P0);
         for(n=j*mP0.column;n<(j+1)*mP0.column;n++){
             for(k=0;k<mP0.weight;k++){
-                H[i][k]=P0[i-(j*mP0.column)][k];
+                H[n][k]=P0[n-(j*mP0.column)][k];
             }
-            H[i][mP0.weight]=d;
+            H[n][mP0.weight]=d;
             d=d+1;
         }
     }
 
+    for(n=0;n<mH.column;n++){
+        for(k=0;k<mH.weight;k++){
+            printf("%d\t",H[n][k]);
+        }
+        printf("\n");
+    }
     //圧縮
     int J=mH.column;
     int s[N-1][J];//圧縮ブロック
 
-    compress(mH,N,M,J,EX[N][M],H[mH.column][mH.weight],s[N-1][J]);
+    compress(mH,N,M,J,EX,H,s);
+    for(n=0;n<N;n++){
+        printf("%d\n",n);
+        for(j=0;j<J;j++){
+            printf("\t%d\n",s[n][j]);
+        }
+    }
 
     
 
